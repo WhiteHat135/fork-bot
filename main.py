166d7f28f4ff8edb55a7ab7816b6c0f71afa719e
feelan <feelan03@gmail.com>
2019-11-02 22:40:08 +0300

    Rewrite full bot

--- main.py
+++ main.py
@@ -1,25 +1,10 @@
 """ Import Module """
-import json
-import request
-import urllib
+import telebot
 """ Main vars """
-token = '869011883:AAFkJ1IEfaqf9bD5RxGekz9L2Xf7z0JCECo'
-url = f"https://api.telegram.org/bot{token}/"
-group_id = "-1001461305516"
-""" Download  """
-def get_url(url):
-    responce = request.get(url)
-    content = responce.content
-    return content
-def get_json_from_url(url):
-    content = get_url(url)
-    js = json.loads(content)
-    return js
-def get_updates():
-    content = url + "getUpdates"
-    js = get_json_from_url(url)
-    return js
-
-    
-""" def send_message(url, group_id, events)
-    responce = request.get(url + "sendMessage?chat_id=-1001461305516&text={events}") """
+chat_id = "-1001461305516"
+""" Profile"""
+bot = telebot.TeleBot("869011883:AAFkJ1IEfaqf9bD5RxGekz9L2Xf7z0JCECo")
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat_id, "Привет, я Меттатон бот. Давай знакомиться!")
+bot.polling()
