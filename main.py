aba8b705e7dc7f509214bfe8bcd635cb5b19194b
feelan <feelan03@gmail.com>
2019-11-02 23:22:18 +0300

    New lib

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 """ Import Module """
-import telebot
+from telegram.ext import Updater
 """ Main vars """
 chat_id = "-1001461305516"
-""" Profile"""
-bot = telebot.TeleBot("869011883:AAFkJ1IEfaqf9bD5RxGekz9L2Xf7z0JCECo")
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat_id, "Привет, я Меттатон бот. Давай знакомиться!")
-bot.polling()
+token = '869011883:AAFkJ1IEfaqf9bD5RxGekz9L2Xf7z0JCECo'
+updater = Updater(token=token, use_context=True)
